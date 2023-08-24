6cea9cafe586cea23d0a678f256efe777149ed86
seemackey <62619768+seemackey@users.noreply.github.com>
2023-08-24 10:04:51 -0400

    data saving, changed stimulus movement

--- main.py
+++ main.py
@@ -4,6 +4,8 @@
 from psychopy import visual, core, event, data, gui
 import os
 import json
+import serial
+port = serial.Serial("COM3",115200) # serial port and baud rate for dell xps laptop
 
 # Create a dialog box to get the participant's name
 dlg = gui.Dlg(title="Participant Information")
@@ -55,10 +57,18 @@ redBox = visual.Rect(
     fillColor='red'
 )
 
-# Create a list of trials for each box
-green_trials = data.TrialHandler(
-    trialList=[{}],
-    nReps=30,  # Number of repetitions for each color
+# Create a list of colors and the number of repetitions for each color
+colors_with_reps = [("green", 10), ("red", 10)]
+
+# Create a list of trials for each color based on repetitions
+color_trials = []
+for color, reps in colors_with_reps:
+    color_trials.extend([{"color": color}] * reps)
+
+# Create a TrialHandler using the color trials
+trials = data.TrialHandler(
+    trialList=color_trials,
+    nReps=1,  # Since repetitions are already handled in the trial list
     method='sequential'
 )
 
@@ -78,7 +88,8 @@ exp_data = data.ExperimentHandler(
 mouse = event.Mouse(win=win)
 
 # Main experiment loop
-for trial in green_trials:
+trial_data_list = []  # List to store trial data dictionaries
+for trial in trials:
     trial_count += 1
 
     # Move the box slightly on each trial
@@ -87,7 +98,8 @@ for trial in green_trials:
     # Draw the current box on the screen
     current_box.draw()
     win.flip()
-    
+    StimTime = core.getTime()
+
     # Wait for any mouse click
     mouse.clickReset()
     while not any(mouse.getPressed()):  # Wait for any mouse button click
@@ -95,29 +107,36 @@ for trial in green_trials:
 
     # Get mouse position
     mouseX, mouseY = mouse.getPos()
-    print(mouseX)
-    print(mouseY)
 
     # Check if mouse click was within the box
+    responseTime = core.getTime()
     if current_box.contains(mouseX, mouseY):
         correct_touch = 1
         correct_touch_count += 1
+        tNow = core.getTime()
+        responseTime = tNow #update RT
+        port.write(str.encode('r10')) # pulse rew system
     else:
         correct_touch = 0
-    
-    # Construct trial data for adding
+
+
+    # Determine the color name based on the current box's fill color
+    if current_box == greenBox:
+        color_name = "green"
+    else:
+        color_name = "red"
+
+    # Construct trial data dictionary
     trial_data = {
-        "box_color": "green",
+        "box_color": color_name,
         "correct_touch": correct_touch,
-        "correct_touch_count": correct_touch_count
+        "correct_touch_count": correct_touch_count,
+        "StimTime": StimTime,
+        "RT": responseTime,
+        "participant": participant_name
     }
 
-    # Add each key-value pair from trial_data separately
-    for key, value in trial_data.items():
-        exp_data.addData(key, value)
-
-    # Append performance data
-    performance_data.append(correct_touch_count / trial_count)
+    trial_data_list.append(trial_data)  # Append trial data to the list
 
     # Provide feedback
     feedback_text = visual.TextStim(win, text=f"Correct touches: {correct_touch_count}/10", pos=(0, -300))
@@ -128,7 +147,7 @@ for trial in green_trials:
     # Clear the screen
     win.flip()
 
-   # If 7 or more correct touches, switch to the other box
+    # If 7 or more correct touches, switch to the other box
     if correct_touch_count >= 7:
         if current_box == greenBox:
             current_box = redBox
@@ -140,6 +159,11 @@ for trial in green_trials:
     # Check for escape key press to exit the experiment
     if 'escape' in event.getKeys():
         break
+
+
+# Add the list of trial data dictionaries to the experiment data handler
+for trial_data in trial_data_list:
+    exp_data.addData(**trial_data)  # Use ** to unpack the dictionary into keyword arguments
 
 # Specify the folder name for saving data
 data_folder = "data"  # Update this to your desired folder name
